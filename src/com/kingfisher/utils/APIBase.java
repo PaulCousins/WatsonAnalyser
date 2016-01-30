2507f062323d28b1ce48b9b0d4e9a6580e185493
Ross Stanton <ross_stanton@hotmail.com>
2016-01-30 18:49:51 +0000

    Re-added key, it's compromised anyway. Will generate a new one...

--- src/com/kingfisher/utils/APIBase.java
+++ src/com/kingfisher/utils/APIBase.java
@@ -2,7 +2,7 @@ package com.kingfisher.utils;
 
 public class APIBase {
 	protected static final String ERROR = "ERROR";
-	protected static final String APIKEY = "REMOVED";
+	protected static final String APIKEY = "965cdce2031d12405fca4ba52596420900532c6b";
 	protected static final String WATSON_URL = "https://gateway-a.watsonplatform.net/";
 			
 			
